b562bf7d72d0831cff8827b6a018f21dc78064a8
Spacecupcake1 <jegathr1@RBAMW091610.emea.roche.com>
2024-09-26 21:38:56 +0200

    Activity Test Passes

--- test/models/activity_test.rb
+++ test/models/activity_test.rb
@@ -1,7 +1,37 @@
-require "test_helper"
+require 'test_helper'
 
 class ActivityTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:one)
+    @trackable = users(:two)  # Using another user as trackable for this example
+    @activity = Activity.new(user: @user, trackable: @trackable, action: "test")
+  end
+
+  test "should be valid" do
+    assert @activity.valid?
+  end
+
+  test "should belong to user" do
+    assert_respond_to @activity, :user
+    assert_equal @user, @activity.user
+  end
+
+  test "should belong to trackable" do
+    assert_respond_to @activity, :trackable
+    assert_equal @trackable, @activity.trackable
+  end
+
+  test "should have polymorphic trackable" do
+    assert_respond_to @activity, :trackable_type
+    assert_equal "User", @activity.trackable_type
+  end
+
+  test "should paginate with 20 per page" do
+    assert_equal 20, Activity.default_per_page
+  end
+
+  test "description should return correct string" do
+    expected_description = "#{@user.user_name} testd a user"
+    assert_equal expected_description, @activity.description
+  end
 end
